a09e95a53b5137127d4ae348dcee56a69f4fc360
Abdirahman Jama <abdirahman.jamaaa@gmail.com>
2021-12-20 18:43:40 +0000

    setup dev environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit', //enables justintime for tailwindcss builds
+    content: [
+        // './public/**/*.html',
+        // './src/**/*.{js,jsx,ts,tsx,vue}',
+        './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
