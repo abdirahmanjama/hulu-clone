357931eb9ecebc0f7a007bdcf6c925035460bd25
Abdirahman Jama <abdirahman.jamaaa@gmail.com>
2021-12-21 20:44:52 +0000

    create header item, add animation css & group content

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
-
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline"
+import HeaderItem from './HeaderItem'
 function Header() {
     return (
         <header className="">
             <h1>This is the header</h1>
+            <div>
+                <HeaderItem label="Home" Icon={HomeIcon}/>
+            </div>
             <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100}/>
 
             {/* Lazy loading, 
