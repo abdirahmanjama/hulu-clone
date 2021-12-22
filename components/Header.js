080c13c6b80545356eaa5ddb02a7ebac91abe71b
Abdirahman Jama <abdirahman.jamaaa@gmail.com>
2021-12-22 13:02:26 +0000

    complete header items component

--- components/Header.js
+++ components/Header.js
@@ -9,12 +9,23 @@ import {
     UserIcon,
 } from "@heroicons/react/outline"
 import HeaderItem from './HeaderItem'
+
+
+const labels = {
+    "Home" : HomeIcon,
+    "Trending": LightningBoltIcon,
+    "Verified": BadgeCheckIcon,
+    "Collections": CollectionIcon, 
+    "Search": SearchIcon,
+    "Account": UserIcon
+}
+//object entries ...
 function Header() {
     return (
-        <header className="">
-            <h1>This is the header</h1>
-            <div>
-                <HeaderItem label="Home" Icon={HomeIcon}/>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+           
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                {Object.entries(labels).map((entry) => <HeaderItem label={entry[0]} Icon={entry[1]}/>)}
             </div>
             <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100}/>
 
